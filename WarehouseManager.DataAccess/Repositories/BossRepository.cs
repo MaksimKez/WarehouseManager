3e4579bf31b2f4e29f3db85f026b665aa868f813
MaksimKez <prostomaxim765@gmail.com>
2024-09-14 18:54:00 +0200

    BossRepository and interface r done
    :)

--- WarehouseManager.DataAccess/Repositories/BossRepository.cs
+++ WarehouseManager.DataAccess/Repositories/BossRepository.cs
@@ -1,6 +1,64 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using WarehouseManager.Database;
+using WarehouseManager.Database.Entities;
+
 namespace WarehouseManager.DataAccess.Repositories;
 
-public class BossRepository
+public class BossRepository : IBossRepository
 {
-    
+    private readonly ApplicationDatabaseContext _context;
+
+    public BossRepository(ApplicationDatabaseContext context)
+    {
+        _context = context ?? throw new ArgumentException("Context error", nameof(context));
+    }
+
+    public async Task<BossEntity> GetByIdAsync(Guid id)
+    {
+        var entity = await _context.Bosses.FirstOrDefaultAsync(b => b.Id == id);
+        if (entity == null)
+            throw new ArgumentException("Boss with given Id is not found", nameof(id));
+        
+        return entity;
+    }
+    public async Task<BossEntity> GetByNameAsync(string name)
+    {
+        var entity = await _context.Bosses.FirstOrDefaultAsync(b => b.Name == name);
+        if (entity == null)
+            throw new ArgumentException("Boss with given name is not found", nameof(name));
+        
+        return entity;
+    }
+
+    public async Task<IEnumerable<BossEntity>> GetFilteredByDateOfRegAsync(DateTime dateOfReg)
+    {
+        return await _context.Bosses.Where(b => b.CreatedAt.Date == dateOfReg.Date).ToListAsync();
+    }
+
+    public async Task AddNewAsync(BossEntity boss)
+    {
+        await _context.Bosses.AddAsync(boss);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task UpdateAsync(BossEntity boss)
+    {
+        var existingBoss = await _context.Bosses.FindAsync(boss.Id);
+        if (existingBoss == null)
+            throw new ArgumentException("Boss with given Id is not found", nameof(boss.Id));
+
+        _context.Entry(existingBoss).CurrentValues.SetValues(boss);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task DeleteAsync(int id)
+    {
+        var boss = await _context.Bosses.FindAsync(id);
+        if (boss == null)
+            throw new ArgumentException("Boss with given Id is not found", nameof(id));
+
+        _context.Bosses.Remove(boss);
+        await _context.SaveChangesAsync();
+    }
 }
--- WarehouseManager.DataAccess/Repositories/IBossRepository.cs
+++ WarehouseManager.DataAccess/Repositories/IBossRepository.cs
@@ -1,11 +1,13 @@
+using WarehouseManager.Database.Entities;
+
 namespace WarehouseManager.DataAccess.Repositories;
 
 public interface IBossRepository
 {
-    //get by id/name and etc ASYNC
-    //get fitered by date of reg
-    //post add new async (auth)       (OAUTH)
-    //update async
-    //delete async, 
-    // everything should return actionRes
+    Task<BossEntity> GetByIdAsync(Guid id);
+    Task<BossEntity> GetByNameAsync(string name);
+    Task<IEnumerable<BossEntity>> GetFilteredByDateOfRegAsync(DateTime dateOfReg);
+    Task AddNewAsync(BossEntity boss);
+    Task UpdateAsync(BossEntity boss);
+    Task DeleteAsync(int id);
 }
